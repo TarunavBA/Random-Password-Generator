a6f97e350642c35dc81b79f0d393c678bcefe67a
Tarunav BA <tarunavba@gmail.com>
2021-09-04 19:42:18 +0530

    📦 NEW: Added the program.

---
+++ Random-password-generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the random password generator!")
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+alphabets = ["a", "B", "c", "D", "e", "F", "g", "H", "i", "J", "k", "L", "m", "N", "o", "P", "q", "R", "s", "T", "u", "V", "w", "X", "y", "Z"]
+
