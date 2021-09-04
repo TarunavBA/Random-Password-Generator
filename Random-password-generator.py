484c41130a9acbd53042fdc03f8eb71086b81ac6
Tarunav BA <tarunavba@gmail.com>
2021-09-04 20:30:17 +0530

    👌 IMPROVE: The program

--- Random-password-generator.py
+++ Random-password-generator.py
@@ -1,5 +1,13 @@
+import random
+
 print("Welcome to the random password generator!")
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-alphabets = ["a", "B", "c", "D", "e", "F", "g", "H", "i", "J", "k", "L", "m", "N", "o", "P", "q", "R", "s", "T", "u", "V", "w", "X", "y", "Z"]
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+#alphabets = []
 
+number = input("How much in letters should be your password?: ")
+
+for i in numbers:
+    random.randint(0, numbers)
+
+print(i) 
